587655906b1e74543f208f4b9a40108b50e319d7
Chris <chrislishuanghong@gmail.com>
2021-05-11 10:39:36 +1000

    fix issue: inconsistent use of tabs and spaces in indentation

--- sync.py
+++ sync.py
@@ -87,7 +87,7 @@ def download_summaries_file(orcid_to_sync):
 	try:
 		suffix = orcid_to_sync[-3:]
 		prefix = suffix + '/' + orcid_to_sync + '.xml'
-        create_folder(path + 'summaries/')
+		create_folder(path + 'summaries/')
 		cmd = 'aws s3 cp ' + summaries_bucket + prefix + ' ' + path + 'summaries/' + prefix + ' --only-show-errors'
 		subprocess.call(shlex.split(cmd), shell=False)
 		record_dict = {}
@@ -120,7 +120,7 @@ def download_activities_file(orcid_to_sync):
 	try:
 		suffix = orcid_to_sync[-3:]
 		prefix = suffix + '/' + orcid_to_sync + '/'
-        create_folder(path + 'activities/')
+		create_folder(path + 'activities/')
 		local_directory = path + 'activities/' + prefix
 		# fetch data from S3
 		logger.info('aws s3 sync ' + activities_bucket + prefix + ' ' + local_directory + ' --delete')
@@ -172,8 +172,7 @@ if __name__ == "__main__":
 		last_sync = (datetime.now() - timedelta(days=30))
 
 	logger.info('Sync records modified after %s', str(last_sync))
-    
-    create_folder(path)
+	create_folder(path)
 
 	last_modified_df = pd.read_csv("last_modified.csv", encoding="UTF-8", header = 0)
 	last_modified_df["last_modified"] = pd.to_datetime(last_modified_df["last_modified"], format=date_format)
